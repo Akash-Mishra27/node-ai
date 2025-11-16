d4513319662e9fc712a199827274dc94e5ebfbab
Akash-Mishra27 <akash.36mishra94@gmail.com>
2025-11-16 17:26:32 +0530

    Added openai basic connection

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+import OpenAI from "openai";
 const project="node-ai"
 console.log(`Project Name: ${project}`);
+const apiKey = process.env.OPEN_AI
+console.log(`Project Name: ${apiKey}`);
+
+
+const openai = new OpenAI({apiKey: apiKey});
+
+const response = await openai.responses.create({
+  model: "gpt-4o",
+  messages: [
+    {
+      role: "user",
+      content: "Write a hello world program in python",
+    },
+  ],
+});
+
+console.log(response);
