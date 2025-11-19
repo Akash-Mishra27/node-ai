77a584ec10288b70718b103b45d4a3872e37424a
Akash-VR46 <akash.36mishra94@gmail.com>
2025-11-19 15:36:41 +0530

    terminal chatbot

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
 import { encoding_for_model } from "tiktoken";
-
 dotenv.config();
 
-
-const project="node-ai"
-console.log(`Project Name: ${project}`);
-
-
-const openai = new OpenAI({apiKey: process.env.OpenAI_Key});
-
-// const response = await openai.responses.create({
-//   instructions: "Provide the color of the given fruit.",
-//   model: "gpt-4o",
-//   input:"color of apple",
-// });
-const prompt = "Explain the theory of relativity in simple terms.";
-const model = "gpt-4o";
-const responses = await openai.responses.create({
-  model: model,
-  input: [
-    // { role: "system", content: "answer in hindi language" },
-    // { role: "developer", content: "gave a basic example in js" },
-    { role: "user", content: prompt }
-  ],
-});
-
-const response = await openai.chat.completions.create({
-  model: "gpt-4o",
-  messages: [
-    { role: "system", content: "You are a helpful assistant that provides concise answers." },
-    { role: "user", content: "What is the capital of France?" }
-  ],
-  max_tokens: 50,
+const client = new OpenAI({
+  apiKey: process.env.OpenAI_Key,
 });
 
 
-function countTokens(text, model = "gpt-4o") {
-  const encoder = encoding_for_model(model);
-  const tokenCount = encoder.encode(text).length;
-  encoder.free();
-  return tokenCount;
+async function aiAnswer(question) {
+  const response = await client.responses.create({
+    model: "gpt-4o-mini",
+    input: question,
+  });
+
+  console.log(response.output_text)
 }
-console.log(response);
+process.stdout.write("Ask me anything: ");
+process.stdin.on("data", (data) => {
+  const question = data.toString().trim();
+  if(question.toLowerCase() === "exit") {
+    process.exit();
+  }
+  aiAnswer(question);
+});
+
