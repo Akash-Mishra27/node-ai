232d5846dbd95abd5cea1a594397ad9cea64ff29
Akash-VR46 <akash.36mishra94@gmail.com>
2025-11-20 17:12:33 +0530

    Model upgradation for image

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ const client = new OpenAI({
 
 async function main() {
   const response = await client.images.generate({
-    model: "dall-e-3",
+    model: "gpt-image-1",
     prompt: "A futuristic cityscape at sunset, with flying cars and towering skyscrapers, in the style of cyberpunk art",
-    size: "1024x1024",
-    response_format: "b64_json",
     n: 1,
   });
   console.log(response.data);
