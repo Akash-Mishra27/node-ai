e12ce08c915c3d9ce22dd522eecef72e08be3ba9
Akash-VR46 <akash.36mishra94@gmail.com>
2025-11-21 12:57:09 +0530

    Stream in browser

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 import {GoogleGenAI} from '@google/genai'
+import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const googleGenAI = new GoogleGenAI({
   apiKey: process.env.Gemini_Key || '',
 })
+const app = express()
 
 
-async function main() {
-  const response  = await googleGenAI.models.generateContent({
+app.get('/', async(req, res) => {
+  const response  = await googleGenAI.models.generateContentStream({
     model: 'gemini-2.5-flash',
-    contents: "Tell me about india",
-    config: {
-      systemInstruction: "tell me in 50 words",
-      // maxOutputTokens: 1024,
-      temperature: 2.0,
-      thinkingConfig: {
-        // enabled: true,
-        includeThoughts: true,
-      },
-    },
+    contents: "Tell me about ai in details",
   })
-  console.log(response.candidates[0].content);
-}
-main()
+
+  for await (const chunk of response) {
+    const text = chunk.text
+    if (text) res.write(text)
+    // console.log(text);
+  }
+  res.send("Check your console")
+  // console.log(response.candidates[0].content);
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
